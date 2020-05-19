c47920c50ae89132502ebd9643800b6e60c58929
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-05-19 22:10:30 +0200

    practice done

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".burger").on("click", function () {
+    $(".fas, aside").toggleClass("show");
+})
+
+
+// const burger = document.querySelector(".burger");
+
+// const iconBurger = document.querySelector(".fa-bars");
+// const iconX = document.querySelector(".fa-times");
+// const column = document.querySelector("aside");
+
+// burger.addEventListener("click", function () {
+//     iconBurger.classList.toggle("show");//tak
+//     iconX.classList.toggle("show");//nie
+//     column.classList.toggle("show");//nie
+
+// })
